1c90714b4b8bbe9bd9f92723b426cdbc586918f3
Foutock Jacques <foutockymgajacquesleroi@gmail.com>
2025-05-07 09:27:18 +0100

    avis clients terminer et responsive

--- cds-site-vitrine/src/components/Avis_Clients.jsx
+++ cds-site-vitrine/src/components/Avis_Clients.jsx
@@ -10,28 +10,12 @@ import React from 'react'
 
 function Avis_Clients() {
     return (
-        <div className='h-fit w-full px-4 my-4 '>
+        <div className='h-fit w-full px-4 my-4 flex flex-wrap'>
             <h1 className='text-blue-500 font-extrabold font-sans md:text-5xl sm:text-4xl text-3xl m-6'>Leurs Avis</h1>
+
             <div className='flex flex-wrap'>
-                <div className='p-1 shadow-lg rounded-sm max-sm:w-full w-[33.3%] h-fit '>
-                    <div className='flex justify-center items-center gap-19 px-14 p-4 border-b-[3px] border-black flex-wrap'>
-                        <img className='clip -ml-14' src={'images3.jpg'} height={100} width={100} />
-                        <div className='-ml-14'>
-                            <h4><span className='font-bold'>Nom</span>:FRANCK</h4>
-                            <p><span className='font-bold'>Proffession</span>: UIUHEIUIUN</p>
-                        </div>
-                    </div>
-                    <div className='flex flex-col justify-center items-center p-4'>
-                        <i><p className='font-light text-xl'>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                            Nam voluptatem ex deleniti voluptatum fugiat harum consectetur asperiores dignissimos, excepturi
-                            eligendi reprehenderit labore maiores natus delectus quia ipsam quam deserunt! Laborum.
-                        </p></i>
-                    </div>
-                </div>
 
-
-                <div className='p-1 shadow-md rounded-sm max-sm:w-full w-[33.3%] h-fit '>
+                <div className='p-1 shadow-md rounded-sm max-[900px]:w-full w-[33.3%] h-fit '>
                     <div className='flex justify-center items-center gap-19 px-16 p-4 border-b-[3px] border-black flex-wrap'>
                         <img className='clip -ml-14' src={'images3.jpg'} height={100} width={100} />
                         <div className='-ml-14'>
@@ -49,7 +33,25 @@ function Avis_Clients() {
                 </div>
 
 
-                <div className='p-1 shadow-md rounded-sm max-sm:w-full w-[33.3%] h-fit '>
+                <div className='p-1 shadow-md rounded-sm max-[900px]:w-full w-[33.3%] h-fit '>
+                    <div className='flex justify-center items-center gap-19 px-16 p-4 border-b-[3px] border-black flex-wrap'>
+                        <img className='clip -ml-14' src={'images3.jpg'} height={100} width={100} />
+                        <div className='-ml-14'>
+                            <h4><span className='font-bold'>Nom</span>:FRANCK</h4>
+                            <p><span className='font-bold'>Proffession</span>: UIUHEIUIUN</p>
+                        </div>
+                    </div>
+                    <div className='flex flex-col justify-center items-center p-4'>
+                        <i><p className='font-light text-xl'>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            Nam voluptatem ex deleniti voluptatum fugiat harum consectetur asperiores dignissimos, excepturi
+                            eligendi reprehenderit labore maiores natus delectus quia ipsam quam deserunt! Laborum.
+                        </p></i>
+                    </div>
+                </div>
+
+
+                <div className='p-1 shadow-md rounded-sm max-[900px]:w-full w-[33.3%] h-fit '>
                     <div className='flex justify-center items-center gap-19 px-16 p-4 border-b-[3px] border-black flex-wrap'>
                         <img className='clip -ml-14' src={'images3.jpg'} height={100} width={100} />
                         <div className='-ml-14'>
