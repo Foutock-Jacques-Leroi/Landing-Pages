3add4a81a33ab08c9792cd65f458b0976990df33
Foutock Jacques <foutockymgajacquesleroi@gmail.com>
2025-05-09 12:49:44 +0100

    main done and NotFound page added

---
+++ cds-site-vitrine/src/NotFound.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+
+
+
+
+
+function NotFound() {
+    return (
+        <div className='h-screen w-full flex justify-center items-center'>
+            <h1 className='text-shadow-lg font-extrabold md:text-4xl sm:text-3xl text-2xl text-blue-500 gap-4'> Ou Nous Trouver <span className='font-extrabold text-teal-300 md:text-5xl sm:text-4xl text-3xl '>?</span></h1>
+        </div>
+    )
+}
+
+export default NotFound
--- cds-site-vitrine/src/main.jsx
+++ cds-site-vitrine/src/main.jsx
@@ -1,14 +1,31 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { ToastContainer, toast } from 'react-toastify'
-
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider, } from 'react-router-dom'
+
+
+
+
+
+const route = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <NotFound />
+  },
+  {
+    path: "/internet_plan",
+    element: <Internet_Plan />,
+    errorElement: <NotFound />
+  }
+])
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-    <ToastContainer position={'top-center'} theme={'light'} />
 
+  <StrictMode>
+    <RouterProvider router={route} />
+    <ToastContainer position={'bottom-right'} theme={'light'} />
   </StrictMode>,
 )
