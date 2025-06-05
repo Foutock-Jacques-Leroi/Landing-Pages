59c51a7a4249c607c3c166c682c2af2efc85c65c
Foutock Jacques <foutockymgajacquesleroi@gmail.com>
2025-06-06 00:39:08 +0100

    most complex part done!

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Navbar2 from './components/Navbar2'
 import Navbar from './components/Navbar'
 import Infotext from './components/Infotext'
 import WhoAreWePage from './components/WhoAreWePage'
+import FirstPage from './components/FirstPage'
 
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
       <Navbar />
       <Infotext />
       <WhoAreWePage />
+      <FirstPage />
       <Footer />
 
 
--- src/components/WhoAreWePage.jsx
+++ src/components/WhoAreWePage.jsx
@@ -11,27 +11,28 @@ function WhoAreWePage() {
         <div className='w-full h-screen px-4 py-3 bg-red-300'>
             <div className='px-2 m-3'>
                 <div>
-                    <p className='text-center text-3xl font-bold mb-4 pt-5'>WHO ARE WE ON THE <br />CARGO SHIPPING DOMAIN</p>
+                    <p className='font-bold text-center text-orange-400'>About Us</p>
+                    <p className='text-center text-3xl font-extrabold mb-4 pt-4'>WHO ARE WE ON THE <br />CARGO SHIPPING DOMAIN</p>
                     <center><hr className='w-30 border-[2px] rounded-full flex items-center justify-center' /></center>
                 </div>
 
 
                 <div className='flex justify-center py-9 items-center max-sm:flex-wrap gap-6'>
 
-                    <div className='h-90 w-full bg-amber-600'>
+                    <div className='h-90 w-full bg-amber-600 rounded-lg shadow-2xl'>
                         image
                     </div>
 
 
                     <div className='w-full h-fit'>
-                        <h3 className='text-shadow-black text-xl md:text-3xl sm:text-2xl py-5'>How we get that experience.</h3>
-                        <p>Lorem ipsum dolor, sit amet consectetur ad
+                        <h3 className='text-black text-shadow-lg font-light text-xl md:text-3xl sm:text-2xl py-5'>How we got that experience.</h3>
+                        <p className='px-6'>Lorem ipsum dolor, sit amet consectetur ad
                             ipisicing elit. Ea, accusamus! Ad ullam asperiores quibusdam voluptas nam, vero eos vitae sequi, 
-                            illo veniam minus itaque unde expedita aperiam provident dignissimos quos?</p>
-                        <p>Lorem ipsum dolor, sit amet consectetur ad
+                            illo veniam minus itaque unde expedita aperiam provident dignissimos quos?
+                        Lorem ipsum dolor, sit amet consectetur ad
                             ipisicing elit. Ea, accusamus! Ad ullam asperiores quibusdam voluptas nam, vero eos vitae sequi, 
-                            illo veniam minus itaque unde expedita aperiam provident dignissimos quos?</p>
-                        <p>Lorem ipsum dolor, sit amet consectetur ad
+                            illo veniam minus itaque unde expedita aperiam provident dignissimos quos?
+                        Lorem ipsum dolor, sit amet consectetur ad
                             ipisicing elit. Ea, accusamus! Ad ullam asperiores quibusdam voluptas nam, vero eos vitae sequi, 
                             illo veniam minus itaque unde expedita aperiam provident dignissimos quos?</p>
                     </div>
